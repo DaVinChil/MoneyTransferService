80bd314843b66ec93152cff8a784c21177cb06c1
DaVinChil <72338867+DaVinChil@users.noreply.github.com>
2023-07-22 18:25:35 +0300

    Update TransferLogger.java
    synchronized logging

--- src/main/java/com/davinci/moneytransferservice/logger/TransferLogger.java
+++ src/main/java/com/davinci/moneytransferservice/logger/TransferLogger.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TransferLogger {
-    private Logger logger = LogManager.getLogger(TransferLogger.class);
+    private final Logger logger = LogManager.getLogger(TransferLogger.class);
     private final double COMMISSION = 10000;
     private final double RUB_RATIO = 100;
     private final String SUCCESS = "SUCCESS";
@@ -36,7 +36,7 @@ public class TransferLogger {
         logTransfer(id, transfer, true);
     }
 
-    public void logTransfer(String id, Transfer transfer, boolean success){
+    public synchronized void logTransfer(String id, Transfer transfer, boolean success){
         logger.log(TRANSFER_LEVEL, String.format("%5s %s %s %13.2f %13.2f %-5s %s",
                 id,
                 transfer.cardFromNumber(),
